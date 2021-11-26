391b463482d49e46411660ce40b701e2e716ae0c
Hyonchori <gus8cjf@naver.com>
2021-11-26 11:22:56 +0900

    edit duplicate labels for yolov5

---
+++ make_custom_dataset/for_yolo/edit_duplicate.py
@@ -0,0 +1,29 @@
+import os
+from tqdm import tqdm
+
+
+def check_duplicate(label_dir, edit=False):
+    labels = sorted(os.listdir(label_dir))
+    corrupt_count = 0
+    for l in tqdm(labels):
+        label_path = os.path.join(label_dir, l)
+        is_corrupt = False
+        with open(label_path) as f:
+            data = f.readlines()
+            data_edit = list(set(data))
+            if len(data) != len(data_edit):
+                corrupt_count += 1
+                is_corrupt = True
+
+        if is_corrupt and edit:
+            txt = ""
+            for box in data_edit:
+                txt += box
+            with open(label_path, "w") as f:
+                f.write(txt)
+    print(corrupt_count)
+
+
+if __name__ == "__main__":
+    label_dir = "/media/daton/D6A88B27A88B0569/dataset/crowdhuman/valid/labels"
+    check_duplicate(label_dir, True)
