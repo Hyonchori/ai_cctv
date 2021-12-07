7aa7058645460e1f5e4a9648b9daead5ba3ac8d6
Hyonchori <gus8cjf@naver.com>
2021-12-07 14:51:38 +0900

    aihub fire dataset edit

--- make_custom_dataset/aihub_dataset/check_aihub_dataset(fire).py
+++ make_custom_dataset/aihub_dataset/check_aihub_dataset(fire).py
@@ -2,6 +2,7 @@ import os
 import json
 
 import cv2
+from tqdm import tqdm
 
 
 class Colors:
@@ -52,9 +53,74 @@ def show_target_idx(root, target, idx, resize=(1280, 720)):
         cv2.waitKey(0)
 
 
+def make_yolov5_dataset_sep(root, target, idx, save_dir, save=False, train=True):
+    if train:
+        img_dir = os.path.join(root, f"[원천]{target}_{idx}") if idx else os.path.join(root, f"[원천]{target}")
+        label_dir = os.path.join(root, f"[라벨]{target}")
+    else:
+        img_dir = os.path.join(root, f"[원천]{target}{idx}") if idx else os.path.join(root, f"[원천]{target}")
+        label_dir = os.path.join(root, f"[라벨]1.{target}")
+    save_image_path = os.path.join(save_dir, "images")
+    if not os.path.exists(save_image_path):
+        os.mkdir(save_image_path)
+    save_label_path = os.path.join(save_dir, "labels")
+    if not os.path.exists(save_label_path):
+        os.mkdir(save_label_path)
+
+    imgs = [x for x in sorted(os.listdir(img_dir)) if x.endswith(".jpg")]
+    for img_name in tqdm(imgs):
+        img_path = os.path.join(img_dir, img_name)
+        img = cv2.imread(img_path)
+        label_name = img_name.replace(".jpg", ".json")
+        label_path = os.path.join(label_dir, label_name)
+        if not os.path.isfile(label_path):
+            continue
+        with open(label_path, encoding="utf-8-sig") as f:
+            annot = json.load(f)
+        img_info = annot["image"]
+        w, h = img_info["resolution"]
+        tmp_annots = annot["annotations"]
+        txt = ""
+        for l in tmp_annots:
+            cls = int(l["class"]) - 1
+            if "box" not in l:
+                continue
+            xyxy = l["box"]
+            cpwhn = xyxy2cpwhn(xyxy, w, h)
+            txt += f"{cls} {cpwhn[0]} {cpwhn[1]} {cpwhn[2]} {cpwhn[3]}\n"
+        tmp_save_image_path = os.path.join(save_image_path, img_name)
+        tmp_save_label_path = os.path.join(save_label_path, img_name.replace(".jpg", ".txt"))
+        if save:
+            cv2.imwrite(tmp_save_image_path, img)
+            with open(tmp_save_label_path, "w") as f:
+                f.write(txt)
+
+
+def xyxy2cpwhn(xyxy, w, h):
+    cpwhn = [round(int(xyxy[0] + xyxy[2]) / 2 / w, 6),
+             round(int(xyxy[1] + xyxy[3]) / 2 / h, 6),
+             round(int(xyxy[2] - xyxy[0]) / w, 6),
+             round(int(xyxy[3] - xyxy[1]) / h, 6)]
+    return cpwhn
+
+
+def make_yolov5_dataset(root, target, save_dir, save=False, train=True):
+    if train:
+        target_indices = [x.split("_")[-1] for x in os.listdir(root) if f"[원천]{target}" in x]
+    else:
+        target_indices = [x[-1] for x in os.listdir(root) if f"[원천]{target}" in x]
+    target_indices = [int(x) if x.isdigit() else "" for x in target_indices]
+    for idx in target_indices:
+        make_yolov5_dataset_sep(root, target, idx, save_dir, save, train)
+
+
 if __name__ == "__main__":
-    root = "/media/daton/D6A88B27A88B0569/dataset/화재 발생 예측 영상/Training"
+    root = "/media/daton/D6A88B27A88B0569/dataset/화재 발생 예측 영상/Validation"
     target = "화재씬"
     idx = 2
 
-    show_target_idx(root, target, idx)
+    #show_target_idx(root, target, idx)
+
+    save_dir = root
+    #make_yolov5_dataset_sep(root, target, idx, save_dir, save=True)
+    make_yolov5_dataset(root, target, save_dir, save=True, train=False)
