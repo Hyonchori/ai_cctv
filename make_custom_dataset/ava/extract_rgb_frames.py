3192d2e70b67ebaf1ac1f5c9a71d2b9e094c17c8
Hyeonchul Jung <gus8cjf@naver.com>
2021-12-27 17:35:18 +0900

    extract rgb frames for ava dataset custom

--- make_custom_dataset/ava/extract_rgb_frames.py
+++ make_custom_dataset/ava/extract_rgb_frames.py
@@ -1,6 +1,8 @@
 import os
+import time
 
 import cv2
+from PIL import Image
 from tqdm import tqdm
 
 
@@ -8,37 +10,55 @@ def extract_rgb_frames_onevid(input_root, vid_name, output_root, save=True, vis=
     input_vid = os.path.join(input_root, vid_name)
     cap = cv2.VideoCapture(input_vid)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    if save:
-        output_dir = os.path.join(output_root, vid_name.split(".")[0])
-        if not os.path.isdir(output_dir):
-            os.mkdir(output_dir)
+    start_frame = 0
+
+    output_dir = os.path.join(output_root, vid_name.split(".")[0])
+    if not os.path.isdir(output_dir):
+        if save:
+            os.makedirs(output_dir)
+    else:
+        saved_frames = [int(x.split(".")[0].split("_")[-1]) for x in os.listdir(output_dir) if x.endswith(".jpg")]
+        start_frame = max(saved_frames)
     print(f"\n--- {vid_name}")
-    print(total_frames)
-    for i in tqdm(range(total_frames)):
+    print(f"{start_frame} ~ {total_frames}")
+    time.sleep(0.1)
+    for i in tqdm(range(start_frame, total_frames)):
+        t1 = time.time()
         ret, img = cap.read()
+        t2 = time.time()
         if not ret:
+            time.sleep(0.1)
             print(f"\ntotal {i} images are extracted")
             break
         if vis:
             cv2.imshow("img", img)
             cv2.waitKey(0)
         if save:
+            img = Image.fromarray(img[..., ::-1])
             tmp_img_name = f"img_{i + 1:05d}.jpg"
             tmp_img_path = os.path.join(output_dir, tmp_img_name)
-            cv2.imwrite(tmp_img_path, img)
+            t1 = time.time()
+            img.save(tmp_img_path)
+            t2 = time.time()
     cap.release()
 
 
 def extract_rgb_frames(input_root, output_root, save=True, vis=False):
-    vid_names = [x for x in os.listdir(input_root) if os.path.isfile(os.path.join(input_root, x))]
+    aleady_names = [x for x in os.listdir(output_root) if os.path.isdir(os.path.join(output_root, x))]
+    vid_names = [x for x in sorted(os.listdir(input_root)) if os.path.isfile(os.path.join(input_root, x))
+                 and x.split(".")[0] not in aleady_names]
+    print(aleady_names)
+    print(vid_names)
     for vid_name in vid_names:
         extract_rgb_frames_onevid(input_root, vid_name, output_root, save, vis)
-
+        break
 
 if __name__ == "__main__":
     input_root = "/media/daton/SAMSUNG/dataset/ava/videos_15min"
     vid_name = "-5KQ66BBWC4.mkv"
-    output_root = "/media/daton/SAMSUNG/dataset/ava/rawframes_e"
+    vid_name = '-FaXLcSFjUI.mp4'
+    #output_root = "/media/daton/SAMSUNG/dataset/ava/rawframes"
+    output_root = "/media/daton/Data/dataset/ava/rawframes"
 
-    #extract_rgb_frames_onevid(input_root, vid_name, output_root, save=True, vis=True)
-    extract_rgb_frames(input_root, output_root, save=True, vis=False)
+    extract_rgb_frames_onevid(input_root, vid_name, output_root, save=True, vis=False)
+    #extract_rgb_frames(input_root, output_root, save=True, vis=False)
