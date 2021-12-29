9ee446ee97579ae70ee93a6ca042488c90350a01
Hyeonchul Jung <gus8cjf@naver.com>
2021-12-29 18:47:10 +0900

    extract frames from video and zip

--- make_custom_dataset/ava/extract_rgb_frames.py
+++ make_custom_dataset/ava/extract_rgb_frames.py
@@ -1,5 +1,7 @@
 import os
 import time
+import zipfile
+import shutil
 
 import cv2
 from PIL import Image
@@ -19,7 +21,7 @@ def extract_rgb_frames_onevid(input_root, vid_name, output_root, save=True, vis=
     else:
         saved_frames = [int(x.split(".")[0].split("_")[-1]) for x in os.listdir(output_dir) if x.endswith(".jpg")]
         start_frame = max(saved_frames)
-    print(f"\n--- {vid_name}")
+    print(f"--- {vid_name}")
     print(f"{start_frame} ~ {total_frames}")
     time.sleep(0.1)
     for i in tqdm(range(start_frame, total_frames)):
@@ -43,27 +45,31 @@ def extract_rgb_frames_onevid(input_root, vid_name, output_root, save=True, vis=
     cap.release()
 
 
-def extract_rgb_frames(input_root, output_root, save=True, vis=False):
-    aleady_names = [x for x in os.listdir(output_root) if os.path.isdir(os.path.join(output_root, x))]
-    vid_names = [x for x in sorted(os.listdir(input_root)) if os.path.isfile(os.path.join(input_root, x))
+def extract_rgb_frames(input_root, output_root, ref_root, save=True, vis=False):
+    aleady_names = [x.split(".")[0] for x in os.listdir(ref_root) if os.path.isfile(os.path.join(ref_root, x))]
+    vid_names = [x for x in os.listdir(input_root) if os.path.isfile(os.path.join(input_root, x))
                  and x.split(".")[0] not in aleady_names]
     print(aleady_names)
     print(vid_names)
-    for vid_name in vid_names:
+    interval = 60
+    for i, vid_name in enumerate(vid_names):
+        print(f"\n############# {i + 1}  / {len(vid_names)}")
         extract_rgb_frames_onevid(input_root, vid_name, output_root, save, vis)
-        break
+        shutil.make_archive(os.path.join(output_root, f"{vid_name.split('.')[0]}"), "zip",
+                            os.path.join(output_root, vid_name.split(".")[0]))
+        shutil.rmtree(os.path.join(output_root, vid_name.split(".")[0]))
+        if (i + 1) % interval == 0:
+            break
+
 
 if __name__ == "__main__":
     input_root = "/media/daton/Data/dataset/ava/videos_15min"
-    input_root = "/media/daton/Data/dataset/ava/target_videos_15min"
+    #input_root = "/media/daton/Data/dataset/ava/target_videos_15min"
     vid_name = "-5KQ66BBWC4.mkv"
     vid_name = '-FaXLcSFjUI.mp4'
     #output_root = "/media/daton/SAMSUNG/dataset/ava/rawframes"
     output_root = "/media/daton/Data/dataset/ava/rawframes"
+    ref_root = "/media/daton/SAMSUNG/dataset/ava/rawframes"
 
     #extract_rgb_frames_onevid(input_root, vid_name, output_root, save=True, vis=False)
-    extract_rgb_frames(input_root, output_root, save=True, vis=False)
-
-    '''
-    cp -r ./-5KQ66BBWC4 /media/daton/SAMSUNG/dataset/ava/rawframes/
-    '''
+    extract_rgb_frames(input_root, output_root, ref_root, save=True, vis=False)
